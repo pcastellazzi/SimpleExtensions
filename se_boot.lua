d1c70415341bb3f81fd5c7d01701261f276e9831
Pablo Castellazzi <pablo.castellazzi@gmail.com>
2017-06-08 02:19:26 -0300

    Enable keyboard chording by default

--- se_boot.lua
+++ se_boot.lua
@@ -1,3 +1,9 @@
+-- http://esoapi.uesp.net/100019/data/k/e/y/KeybindingsManager.IsChordingAlwaysEnabled.html
+-- https://en.wikipedia.org/wiki/Chorded_keyboard
+function KeybindingsManager:IsChordingAlwaysEnabled()
+    return true
+end
+
 EVENT_MANAGER:RegisterForEvent("SimpleExtensions", EVENT_ADD_ON_LOADED, function (event, name)
     if (name == "SimpleExtensions") then
         EVENT_MANAGER:UnregisterForEvent(name, event)
